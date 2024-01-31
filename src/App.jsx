3e6d33535f82ab29d441df7b1b9b56ebe3e81061
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-02-01 00:48:31 +0530

    Created dynamic layout like leetcodee

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home";
 import SignIn from './pages/SignIn';
+import ResizableDiv from './components/Resizeable';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/sign' element={<SignIn></SignIn>} />
         <Route path='/' element={<Home></Home>} />
+        <Route path='/resizeable-divs' element={<ResizableDiv/>}/>
       </Routes>
     </div>
   );
---
+++ src/components/Resizeable.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import '../components/resize.css';
+
+const ResizableDiv = () => {
+  const [verticalSplitterPos, setVerticalSplitterPos] = useState(window.innerWidth / 2);
+  const [horizontalSplitterPos, setHorizontalSplitterPos] = useState(window.innerHeight / 2);
+
+  const handleVerticalMouseDown = (e) => {
+    e.preventDefault();
+    document.addEventListener('mousemove', handleVerticalMouseMove);
+    document.addEventListener('mouseup', handleVerticalMouseUp);
+  };
+
+  const handleVerticalMouseMove = (e) => {
+    setVerticalSplitterPos(e.clientX);
+  };
+
+  const handleVerticalMouseUp = () => {
+    document.removeEventListener('mousemove', handleVerticalMouseMove);
+    document.removeEventListener('mouseup', handleVerticalMouseUp);
+  };
+
+  const handleHorizontalMouseDown = (e) => {
+    e.preventDefault();
+    document.addEventListener('mousemove', handleHorizontalMouseMove);
+    document.addEventListener('mouseup', handleHorizontalMouseUp);
+  };
+
+  const handleHorizontalMouseMove = (e) => {
+    setHorizontalSplitterPos(e.clientY);
+  };
+
+  const handleHorizontalMouseUp = () => {
+    document.removeEventListener('mousemove', handleHorizontalMouseMove);
+    document.removeEventListener('mouseup', handleHorizontalMouseUp);
+  };
+
+  return (
+    <div className="ResizableDiv">
+      <div style={{ width: verticalSplitterPos }} className="left-div">
+        Question
+      </div>
+      <div
+        onMouseDown={handleVerticalMouseDown}
+        style={{ left: verticalSplitterPos }}
+        className="vertical-splitter"
+      />
+      <div style={{ left: verticalSplitterPos + 10 }} className="right-div">
+        <div style={{ height: horizontalSplitterPos }} className="top-div">
+          Code Editor
+        </div>
+        <div
+          onMouseDown={handleHorizontalMouseDown}
+          style={{ top: horizontalSplitterPos }}
+          className="horizontal-splitter"
+        />
+        <div style={{ top: horizontalSplitterPos + 10 }} className="bottom-div">
+          Test Cases
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResizableDiv;
